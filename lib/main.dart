cc3c56b5622f911818fa3a9c049f7ab5b984ec0a
Samuel Nascimento <samuelpn15@gmail.com>
2022-06-30 09:06:56 -0300

    Aplicando técnica de extração de código

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,49 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Column(
-        children: const [
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('R\$100'),
-              subtitle: Text('1234'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('R\$200'),
-              subtitle: Text('1000'),
-            ),
-          ),
-        ],
-      ),
+      body: ListaTransferencia(),
       appBar: AppBar(
         title: const Text('Transferências'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
         child: const Icon(Icons.add),
       ),
     ),
   ));
+}
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      ItemTransferencia(Transferencia(100.0, 1000)),
+      ItemTransferencia(Transferencia(200.0, 1000)),
+      ItemTransferencia(Transferencia(300.0, 1000)),
+    ]);
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
